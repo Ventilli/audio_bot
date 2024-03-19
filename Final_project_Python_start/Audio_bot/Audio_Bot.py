cf1b2da3a0e8c996384c4cf4f37675ff99b66ab4
Ventilli <samir.y10@bk.ru>
2024-03-19 17:59:40 +0300

    РЫШЫР

--- Final_project_Python_start/Audio_bot/Audio_Bot.py
+++ Final_project_Python_start/Audio_bot/Audio_Bot.py
@@ -1,5 +1,6 @@
 from telebot.types import Message
 import speech_recognition as SR
+import telegramApi
 import subprocess
 import requests
 import telebot
@@ -31,8 +32,12 @@ def audio(message_path):
 
 @BOT.message_handler(commands=['start'])
 def welcome(message:Message):
-    BOT.reply_to(message,
-                 f'Привет, {message.chat.username}! Отправь мне аудио сообщение и я переформатирую его в текст!\nУчти, что я работаю только с русским языком (пока)!')
+    if message.chat.username != None:
+        BOT.reply_to(message,
+                     f'Привет, {message.chat.username}! Отправь мне аудио сообщение и я переформатирую его в текст!\nУчти, что я работаю только с русским языком (пока)!')
+    else:
+        BOT.reply_to(message,
+                     f'Привет! Отправь мне аудио сообщение и я переформатирую его в текст!\nУчти, что я работаю только с русским языком (пока)!')
     
     print(message.chat.username)
 
@@ -41,23 +46,14 @@ def audio_formatting(message:Message):
     file_info = BOT.get_file(message.voice.file_id)
     path = file_info.file_path
     file_name = os.path.basename(path)
-    doc = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(TOKEN, file_info.file_path)) # Получаем и сохраняем присланную голосвуху. От автора: Ага, админ может в любой момент отключить удаление айдио файлов и слушать все, что ты там говоришь. А представь, что такую бяку подселят в огромный чат и она будет просто логировать все сообщения [анонимность в телеграмме, ахахаха]
-    with open('Final_project_Python_start/Audio_bot/'+file_name, 'wb') as f:
-        f.write(doc.content)
-    subprocess.run(['c:/Users/Самир/.vscode/ffmpeg.exe', '-i', 'Audio_bot/file_27.mp3', 'Audio_bot/file_27.wav', '-y'])
-    # src_filename = 'Audio_bot/file_27.oga'
-    # dest_filename = 'Audio_bot/fil.wav'
+    downloaded_file = BOT.download_file(path)
+    with open(file_name, 'wb') as f:
+        f.write(downloaded_file)
 
-    # process = subprocess.run(['ffmpeg', '-i', src_filename, dest_filename])
-    
-
-
-
-    # file_info = BOT.get_file(message.voice.file_id)
-    # downloaded_file = BOT.download_file(file_info.file_path)
-    # print(downloaded_file)
-    # print(message.voice)
-    BOT.reply_to(message, format(audio('Audio_bot/file_27.mp3')))
+    print(file_name)
+    time.sleep(1)
+    subprocess.run(['ffmpeg', '-i', file_name, file_name+'.wav', '-y'])
+    BOT.reply_to(message, format(audio(file_name+'.wav')))
 
 # r = SR.Recognizer()
 
