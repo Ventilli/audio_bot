6cb1cdb3f5808645597ed58b9f4641ad4d293759
Ventilli <samir.y10@bk.ru>
2024-06-06 16:36:58 +0300

    Version 2.0
    Два типа языка доступно

--- Final_project_Python_start/Audio_bot/Audio_Bot.py
+++ Final_project_Python_start/Audio_bot/Audio_Bot.py
@@ -6,7 +6,8 @@ import os
 
 
 
-TOKEN = '7161395864:AAHmQxVjFNwDsTRIxHIM8O0RugndRtcY3YA'
+# TOKEN = '7161395864:AAHmQxVjFNwDsTRIxHIM8O0RugndRtcY3YA'
+TOKEN = '7227162273:AAEqAvDOER2NbBuidyc5KMogF-X8zf8h82g'
 BOT = telebot.TeleBot(token = TOKEN)
 
 userLanguage = 'ru_RU'
@@ -31,12 +32,23 @@ def audio(message_path):
 def welcome(message:Message):
     if message.chat.username != None:
         BOT.reply_to(message,
-                     f'Привет, {message.chat.username}! Отправь мне аудио сообщение и я переформатирую его в текст!\nУчти, что я работаю только с русским языком (пока)!')
+                     f"Привет, {message.chat.username}! Отправь мне аудио сообщение и я переформатирую его в текст!\nТы можешь изменить язык сообщения с помощью команды /change\n\n" +
+                     f"Hi, {message.chat.username}! Send me an audio message and I'll reformat it into text!\nYou can change the language of the message using the /change command")
     else:
         BOT.reply_to(message,
-                     'Привет! Отправь мне аудио сообщение и я переформатирую его в текст!\nУчти, что я работаю только с русским языком (пока)!')
+                     "Привет! Отправь мне аудио сообщение и я переформатирую его в текст!\nТы можешь изменить язык сообщения с помощью команды /change\n\n" +
+                     "Hi! Send me an audio message and I'll reformat it into text!\nYou can change the language of the message using the /change command")
 
-    
+@BOT.message_handler(commands=['change'])
+def languageChoose(message:Message):
+    markup = InlineKeyboardMarkup(row_width=2)
+    ru = InlineKeyboardButton('Russian\nРусский', callback_data='ru')
+    en = InlineKeyboardButton('English\nАнглийский', callback_data = 'en')
+    markup.add(ru, en)
+    BOT.send_message(message.chat.id, 'Для начала выбери язык сообщения\nFirst, select the language of the message',
+                    reply_markup=markup)
+
+
 
 @BOT.message_handler(content_types=['voice'])
 def voice_formatting(message:Message):
@@ -74,27 +86,21 @@ def audio_formatting(message:Message):
 
 
 
-@BOT.message_handler(commands=['st'])
-def welcom(message:Message):
-    markup = InlineKeyboardMarkup(row_width=2)
-    ru = InlineKeyboardButton('Russian\nРусский', callback_data='ru')
-    en = InlineKeyboardButton('English\nАнглийский', callback_data = 'en')
-    markup.add(ru, en)
-    BOT.send_message(message.chat.id, 'Для начала выбери язык сообщения\nFirst, select the language of the message',
-                    reply_markup=markup)
-
-
 
 @BOT.callback_query_handler(func = lambda call : True)
 def callback(call : Message):
+    global userLanguage
 
     if call is not None:
 
         if call.data == 'ru':
-            # userLanguage = 'ru_RU'
+            userLanguage = 'ru_RU'
             BOT.send_message(call.message.chat.id, 'Отлично, теперь ваш язык по умолчанию: русский\nGreat, now your default language is Russian')
         if call.data == 'en':
-            # userLanguage = 'en_EN'
+            userLanguage = 'en_EN'
             BOT.send_message(call.message.chat.id, 'Отлично, теперь ваш язык по умолчанию: английский\nGreat, now your default language is English')
+            
+    else:
+        print('Молосольные огурчики')
 
 BOT.polling()
